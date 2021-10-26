3c25f5323666ee3ad19ab49b006ff362ec1a89b9
Lukas Geyrhofer <lukasgeyrhofer@gmx.at>
2021-10-26 21:57:03 +0200

    First Interface Changes / Test Projects

---
+++ TourPlanner/TourPlanner/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace TourPlanner
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+
+}
