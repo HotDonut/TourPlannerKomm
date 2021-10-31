d286d91b729c658e8ab7add79a16237c15a7a4f2
Lukas Geyrhofer <lukasgeyrhofer@gmx.at>
2021-10-31 19:11:23 +0100

    Creating MainWindow, DataContext

--- TourPlanner/TourPlanner/ViewModels/MainViewModel.cs
+++ TourPlanner/TourPlanner/ViewModels/MainViewModel.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 
-namespace TourPlanner
+namespace TourPlanner.ViewModels
 {
     public class MainViewModel : INotifyPropertyChanged
     {
