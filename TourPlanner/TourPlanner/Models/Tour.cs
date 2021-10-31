ec0ddc2da1d1b9e040231c8522546f0b58123906
Lukas Geyrhofer <lukasgeyrhofer@gmx.at>
2021-10-31 20:23:31 +0100

    Adding Tour and Log Models, Dummy Tours

---
+++ TourPlanner/TourPlanner/Models/Tour.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+
+namespace TourPlanner.Models
+{
+    public class Tour
+    {
+        private string _imagePath;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string FromLocation { get; set; }
+        public string ToLocation { get; set; }
+        public int Distance { get; set; }
+        public string ImagePath
+        {
+            get
+            {
+                if (_imagePath == null || _imagePath.Equals(""))
+                    return @".\..\..\..\Images\Icon\No_Image_Icon.png";
+
+                if (File.Exists(_imagePath))
+                    return _imagePath;
+
+                return @".\..\..\..\Images\Icon\No_Image_Icon.png";
+            }
+            set => _imagePath = value;
+        }
+
+        public Tour(int id, string name, string description, string from, string to, int distance, string imagePath)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Description = description;
+            this.FromLocation = from;
+            this.ToLocation = to;
+            this.Distance = distance;
+            this.ImagePath = imagePath;
+        }
+    }
+}
--- TourPlanner/TourPlanner/ViewModels/MainViewModel.cs
+++ TourPlanner/TourPlanner/ViewModels/MainViewModel.cs
@@ -2,13 +2,42 @@
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
+using System.Collections;
+using System.Collections.ObjectModel;
+using TourPlanner.Models;
 
 namespace TourPlanner.ViewModels
 {
-    public class MainViewModel : INotifyPropertyChanged
+    public class MainViewModel : ViewModelBase
     {
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<Tour> Tours { get; set; }
+        public string Name { get; set; }
+        private string higherOrLower;
+
+        public string HigherOrLower
+        {
+            get { return higherOrLower; }
+            set
+            {
+                if (higherOrLower != value)
+                {
+                    higherOrLower = value;
+                    RaisePropertyChangedEvent(nameof(HigherOrLower));
+                }
+            }
+        }
+
+        public MainViewModel()
+        {
+            Tours = new ObservableCollection<Tour>();
+            Tour a = new Tour(0, "Test", "TestTour", "Vienna", "Graz", 200, "");
+            Tour b = new Tour(0, "Test", "TestTour", "Vienna", "Graz", 200, "");
+            Tours.Add(a);
+            Tours.Add(b);
+        }
+
+
 
     }
 
